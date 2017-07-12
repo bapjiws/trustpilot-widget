eafac78ae08be724bf7e79358ac672cde1341a91
bapjiws <alex.boklin@gmail.com>
2017-07-13 00:22:55 +0300

    Refactor code and enable jumping back over several stars

--- scripts/floating_rating.js
+++ scripts/floating_rating.js
@@ -1,44 +1,36 @@
 window.onload = function () {
-    // var stars = [{
-    //     domRef: document.querySelector('.rating-' + '1'),
-    //     left: ratingContainer.clientWidth / 5 * (1 - 1), // 0
-    //     right: ratingContainer.clientWidth / (5 * 1 - 1) // 51
-    // }];
-
-    var oneStar = document.querySelector('.rating-1');
-    var twoStars = document.querySelector('.rating-2');
-    var threeStars = document.querySelector('.rating-3');
-    var fourStars = document.querySelector('.rating-4');
-    var fiveStars = document.querySelector('.rating-5');
-
     var ratingContainer = document.querySelector('.rating-container');
     console.log('ratingContainer:', ratingContainer.clientWidth);
 
-    var widths = new Array(5);
-    [1, 2, 3, 4, 5].forEach(function(step) {
-        widths[step - 1] = ratingContainer.clientWidth / 5 * step;
-    });
-    console.log('widths:', widths);
+    var stars = [1, 2, 3, 4, 5].reduce(function (acc, curr) {
+        acc.push({
+            rating: curr,
+            domRef: document.querySelector('.rating-' + curr),
+            left: (ratingContainer.clientWidth / 5) * (curr - 1),
+            right: (ratingContainer.clientWidth / 5) * curr - 1
+        });
+        return acc;
+    }, []);
+    console.log('stars:', stars);
 
     ratingContainer.addEventListener('mousemove', function(event) {
-        if (event.offsetX < widths[0]) {
-            oneStar.style.zIndex= 1;
-            twoStars.style.zIndex= 0;
-        } else if (event.offsetX > widths[0] && event.offsetX < widths[1]) { // second star
-            oneStar.style.zIndex= 0;
-            twoStars.style.zIndex= 1;
-            threeStars.style.zIndex= 0;
-        } else if (event.offsetX > widths[1] && event.offsetX < widths[2]) {
-            twoStars.style.zIndex= 0;
-            threeStars.style.zIndex= 1;
-            fourStars.style.zIndex= 0;
-        } else if (event.offsetX > widths[2] && event.offsetX < widths[3]) {
-            threeStars.style.zIndex= 0;
-            fourStars.style.zIndex= 1;
-            fiveStars.style.zIndex= 0;
-        } else if (event.offsetX > widths[3] && event.offsetX < widths[4]) {
-            fourStars.style.zIndex= 0;
-            fiveStars.style.zIndex= 1;
-        }
+        // TODO: use a more efficient way of establishing the right interval -- e.g., binary search
+        for (idx in stars) {
+            // console.log('star:', key);
+            if (event.offsetX > stars[idx].left && event.offsetX < stars[idx].right) {
+                console.log('STAR:', idx);
+
+                // console.log('test:', stars.filter(function (star) {return star.rating !== idx + 1}));
+                stars.forEach(function (star) {
+                    if (star.rating === (+idx + 1)) {
+                        // console.log('SELECTED STAR:', star);
+                        star.domRef.style.zIndex = 1;
+                    } else {
+                        // console.log('UNSELECTED STAR:', star);
+                        star.domRef.style.zIndex = 0;
+                    }
+                });
+                break;
+        }}
     });
 };
