4b7466dbfd2a07426799d5ea0ce0f05935228592
bapjiws <alex.boklin@gmail.com>
2017-07-13 11:36:57 +0300

    Restack stars only if the mouse if over a different star

--- scripts/floating_rating.js
+++ scripts/floating_rating.js
@@ -6,31 +6,37 @@ window.onload = function () {
         acc.push({
             rating: curr,
             domRef: document.querySelector('.rating-' + curr),
-            left: (ratingContainer.clientWidth / 5) * (curr - 1),
-            right: (ratingContainer.clientWidth / 5) * curr - 1
+            left: (ratingContainer.clientWidth / 5) * (curr - 1), // left boundary included
+            right: (ratingContainer.clientWidth / 5) * curr - 1 // right boundary excluded
         });
         return acc;
     }, []);
     console.log('stars:', stars);
 
+    var calledAtOffsetX = 0;
+
     ratingContainer.addEventListener('mousemove', function(event) {
-        // TODO: use a more efficient way of establishing the right interval -- e.g., binary search
         for (idx in stars) {
             // console.log('star:', key);
+            // TODO: use a more efficient way of establishing the right interval -- e.g., binary search
             if (event.offsetX > stars[idx].left && event.offsetX < stars[idx].right) {
-                console.log('STAR:', idx);
+                // console.log(calledAtOffsetX, stars[idx].left, stars[idx].right);
 
-                // console.log('test:', stars.filter(function (star) {return star.rating !== idx + 1}));
-                stars.forEach(function (star) {
-                    if (star.rating === (+idx + 1)) {
-                        // console.log('SELECTED STAR:', star);
-                        star.domRef.style.zIndex = 1;
-                    } else {
-                        // console.log('UNSELECTED STAR:', star);
-                        star.domRef.style.zIndex = 0;
-                    }
-                });
-                break;
+                if (calledAtOffsetX <= stars[idx].left || calledAtOffsetX > stars[idx].right) {
+                    calledAtOffsetX = event.offsetX;
+
+                    console.log('STAR:', idx);
+                    stars.forEach(function (star) {
+                        if (star.rating === (+idx + 1)) {
+                            // console.log('SELECTED STAR:', star);
+                            star.domRef.style.zIndex = 1;
+                        } else {
+                            // console.log('UNSELECTED STAR:', star);
+                            star.domRef.style.zIndex = 0;
+                        }
+                    });
+                    break;
+                }
         }}
     });
 };
